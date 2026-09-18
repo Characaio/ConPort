751893768d1f20128485cdbb6983f8b34c7392b3
Characaio <characaioeumesmo@gmail.com>
2026-09-18 20:53:49 -0300

    Inicialização do projeto

---
+++ Backend/src/main/java/com/example/ecoportapi/EcoPortApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecoportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPortApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoPortApiApplication.class, args);
+    }
+
+}
